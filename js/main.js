2ae312033fda3057b7a1549538aaa72dea68c461
Andrew Rock <andrew@deepcovelabs.com>
2016-07-18 20:36:30 +0100

    hello is it me you're looking for

---
+++ js/main.js
@@ -0,0 +1,108 @@
+(function() {
+  'use strict';
+
+  var w = window,
+    d = document,
+    ee = d.documentElement,
+    g = d.getElementsByTagName('body')[0],
+    bodyWidth= w.innerWidth || ee.clientWidth || g.clientWidth,
+    body = document.body;
+
+  if( /Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    var video = document.getElementsByTagName("source");
+    var videos = document.getElementsByTagName("video")
+    for(var i = 0; i < videos.length; i++){
+      video[i].removeAttribute("src");
+
+    }
+  }
+
+  var feed = new Instafeed({
+    accessToken: '15736008.d6bf5a0.bc5b7503355d4e438d2781ece6649ab8',
+    get: 'user',
+    userId: 15736008,
+    sortBy: 'most-liked',
+    resolution: "standard_resolution",
+    template:
+    '<a href="{{link}}" target="_blank">' +
+    '<img src="{{image}}" />' +
+    '</a>' ,
+    filter: function(image) {
+      return image.type === 'image';
+    }
+  });
+
+  (function(){
+    var burger = document.querySelector('.burger-container'),
+        header = document.querySelector('.navigation-area-mobile');
+
+    burger.onclick = function() {
+        header.classList.toggle('open');
+      };
+  }());
+
+  (function(){
+    function onTabClickMobile(event){
+      var actives = document.querySelectorAll('.active'),
+        header = document.querySelector('.navigation-area-mobile');
+
+      // deactivate existing active tab and panel
+      for (var i=0; i < actives.length; i++){
+        actives[i].className = actives[i].className.replace('active', '');
+      }
+
+      // activate new tab and panel
+      event.target.className += ' active';
+      event.preventDefault();
+      document.getElementById(event.target.href.split('#')[1]).className += ' active active-home';
+      header.classList.toggle('open');
+    }
+
+    var el = document.getElementById('mobile-tabs');
+    el.addEventListener('click', onTabClickMobile, false);
+  })();
+
+  (function(){
+    function onTabClick(event){
+      var actives = document.querySelectorAll('.active');
+      for (var i=0; i < actives.length; i++){
+        actives[i].className = actives[i].className.replace('active', '');
+      }
+      event.target.className += ' active';
+      document.getElementById(event.target.href.split('#')[1]).className += ' active';
+    }
+
+    var el = document.getElementById('tabs');
+    el.addEventListener('click', onTabClick, false);
+  })();
+
+  (function(){
+    var counter = 0,
+      items = document.querySelectorAll('.slide'),
+      next = document.querySelector('.next'),
+      previous = document.querySelector('.previous'),
+      numItems = items.length;
+
+    var showCurrent = function(){
+      var itemToShow = Math.abs(counter%numItems);
+      [].forEach.call( items, function(el){
+        el.classList.remove('show');
+      });
+      items[itemToShow].classList.add('show');
+    };
+
+    next.addEventListener('click', function() {
+      counter++;
+      showCurrent();
+    }, false);
+
+    previous.addEventListener('click', function() {
+      counter--;
+      showCurrent();
+    }, false);
+
+  })();
+
+
+  feed.run();
+})();
---
+++ readme.md
@@ -0,0 +1,20 @@
+## Welcome
+
+Personal website, mostly for trying out new things. Brief resume/CV, Instagram feed and videos from my travels.
+
+## About
+
+Work history briefly outlined.
+Videos have been compressed, quality is not the best, working on a better way to stream the videos in higher definition without killing bandwidth.
+
+Will add projects in the near future
+
+## Installation
+
+Multiple versions of videos are in the video folder, feel free to download and reuse.
+If you wish to have higher quality, feel free to contact me.
+
+## API's used
+
+Instafeed.js
+
